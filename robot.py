4cad0eacdc6bbac5575e97bf6636b4d8842e681f
Bw11111 <91095401+Bw11111@users.noreply.github.com>
2022-01-01 10:38:17 -0800

    Update robot.py

--- robot.py
+++ robot.py
@@ -2,7 +2,7 @@ from mindstorms import MSHub, Motor, MotorPair, ColorSensor, DistanceSensor, App
 from mindstorms.control import wait_for_seconds, wait_until, Timer
 from mindstorms.operator import greater_than, greater_than_or_equal_to, less_than, less_than_or_equal_to, equal_to, not_equal_to
 import math
-
+#add another motor
 hub = MSHub()
 motor = Motor('A')
 paper_scanner = ColorSensor('F')
